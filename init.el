64df54d611c00a4fcc8b33036e88e9033c9ea4f0
liefswanson <liefs@sfu.ca>
2015-02-11 18:16:24 -0800

    fix config error with minimap

--- init.el
+++ init.el
@@ -46,7 +46,16 @@
 	 (save-options-on-exit off))))
  '(flycheck-clang-language-standard "c++14")
  '(global-auto-complete-mode t)
-
+ '(initial-scratch-message
+   #(";; This buffer is for notes you don't want to save, and for Lisp evaluation.
+;; If you want to create a file, visit that file with Ctrl+O,
+;; then enter the text in that file's own buffer." 131 135
+													   (face ergoemacs-pretty-key)
+													   136 137
+													   (face ergoemacs-pretty-key)))
+ '(minimap-always-recenter nil)
+ '(minimap-recenter-type (quote middle))
+ '(minimap-update-delay 0.4)
  '(minimap-width-fraction 0.13)
  '(minimap-window-location (quote right))
  '(org-CUA-compatible t)
